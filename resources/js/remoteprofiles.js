e434d8f39b3d0b8155143df94259b82beb5df683
Konstantin <kulik.konstantin@gmx.de>
2020-08-18 11:47:55 +0200

    Kurbeschreibung wird bei remote profile abgekürzt und fügt link ein

--- resources/js/remoteprofiles.js
+++ resources/js/remoteprofiles.js
@@ -2,6 +2,7 @@ let allProfileContainers = document.getElementsByClassName("profile");
 let searchInput = document.getElementById("search-input");
 let clearButton = document.getElementById("search-bar-icon-clear");
 searchRemoteProfiles();
+reduceKurzbeschreibung();
 
 function searchRemoteProfiles() {
   let query = searchInput.value.toLowerCase();
@@ -28,4 +29,19 @@ function clearInput(){
   searchInput.value = "";
   clearButton.classList.add("hidden");
   searchRemoteProfiles();
+}
+
+//Kurzbeschreibungen per JS anpassen?
+function reduceKurzbeschreibung(){
+  for (i = 0; i < allProfileContainers.length; i++) {
+    let currentKurzbeschreibungElement = allProfileContainers[i].querySelector(".kurzbeschreibung");
+    let currentKurzbeschreibungString = currentKurzbeschreibungElement.textContent;
+    if (currentKurzbeschreibungString.length > 300){
+      let shortedString = currentKurzbeschreibungString.substring(0, 300);
+      let link = allProfileContainers[i].querySelector("a").getAttribute("href");
+
+      currentKurzbeschreibungElement.innerHTML = shortedString + "... <a href='"
+                                + link + "'>(weiter lesen)</a>";
+    }
+  }
 }
